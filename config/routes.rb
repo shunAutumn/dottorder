4b2efad71e43e1d69610fd461dbb5415ddf57493
shunAutumn <tarosan.programming@gmail.com>
2020-11-28 23:13:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  get 'mypage', to: 'posts#mypage'
   root "posts#index"
   resources :users, only: [:edit, :update]
-  
+  resources :posts
 end
