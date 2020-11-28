abdaa10be1972e440f99bcb97acb074a48451ac5
shunAutumn <tarosan.programming@gmail.com>
2020-11-28 23:13:51 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_115412) do
+ActiveRecord::Schema.define(version: 2020_11_24_113312) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "toptitle", null: false
+    t.text "topdescription", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.text "url"
+    t.text "image"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_11_22_115412) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
